713c7a522416439df80cfaf5c32af142a39a5112
NoOrdinaryGuy <neil.ord@mclaren.com>
2015-05-06 14:34:36 +0100

    WIP: Move to akka-http-RC2 API level.

--- src/main/scala/io/neilord/BootWithFlow.scala
+++ src/main/scala/io/neilord/BootWithFlow.scala
@@ -1,72 +1,100 @@
 package io.neilord
 
 import akka.actor.ActorSystem
-import akka.stream.FlowMaterializer
+import akka.http.scaladsl.Http
+import akka.stream._
 import scala.concurrent.Future
-import akka.http.model._
-import akka.http._
 import scala.concurrent.ExecutionContext.Implicits.global
 import akka.stream.scaladsl._
 import scala.util.Random
-import akka.http.model.HttpResponse
-import akka.stream.scaladsl.FlowGraphImplicits._
+import akka.http.scaladsl.model._
 
 object BootWithFlow extends App with BSONUtils {
 
   implicit val system = ActorSystem("Streams")
-  implicit val materializer = FlowMaterializer()
+  implicit val materializer = ActorFlowMaterializer()
 
   //Start the server
-  val serverBinding = Http().bind(interface = "localhost", port = 8090)
-  serverBinding.connections.foreach { connection =>
-    println(s"Connection accepted from ${connection.remoteAddress}")
-//    connection handleWith { broadCastZipFlow }
-    connection handleWith { broadCastMergeFlow }
-  }
+  val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
+    Http().bind(interface = "localhost", port = 8091)
+
+  val bindingFuture: Future[Http.ServerBinding] = serverSource.to(Sink.foreach {
+    connection =>
+      println(s"Connection accepted from ${connection.remoteAddress}")
+      //connection handleWith { broadcastZipFlow }
+      connection handleWith { broadcastMergeFlow }
+  }).run()
 
   //Some dummy steps that totally disregard the request, and return an entry of their choosing.
-  val step1 = Flow[HttpRequest].mapAsync[String](getTickerHandler("GOOG"))
-  val step2 = Flow[HttpRequest].mapAsync[String](getTickerHandler("AAPL"))
-  val step3 = Flow[HttpRequest].mapAsync[String](getTickerHandler("MSFT"))
+  val step1 = Flow[HttpRequest].mapAsync[String](1)(getTickerHandler("GOOG"))
+  val step2 = Flow[HttpRequest].mapAsync[String](1)(getTickerHandler("AAPL"))
+  val step3 = Flow[HttpRequest].mapAsync[String](1)(getTickerHandler("MSFT"))
 
-  val mapStringToResponse = Flow[String].map[HttpResponse](
+  val mapStringToResponse: Flow[String, HttpResponse, Unit] = Flow[String].map[HttpResponse](
     (in: String) => HttpResponse(status = StatusCodes.OK, entity = in)
   )
 
-  //These will be plumbed into the source and sink of the HTTP connection, we leave them
-  //undefined here.
-  val in = UndefinedSource[HttpRequest]
-  val out = UndefinedSink[HttpResponse]
-
   //"To Many"
-  val bCast = Broadcast[HttpRequest]
+  val bCast3 = Broadcast[HttpRequest](3)
 
   //"From Many" - Merge takes the first available response
-  val merge = Merge[String]
+  val merge3 = Merge[String](3)
 
   //"From Many" - ZipWith combines the inputs (3 here) into some result (HttpResponse). Set them using .inputX.
-  val zip = ZipWith[String, String, String, HttpResponse] {
-    (inp1, inp2, inp3) => new HttpResponse(status = StatusCodes.OK,entity = inp1 + inp2 + inp3)
+  val zip3 = ZipWith[String, String, String, HttpResponse] {
+    (inp1, inp2, inp3) => new HttpResponse(status = StatusCodes.OK, entity = inp1 + inp2 + inp3)
   }
 
-  //There's a faked delay in step1-3, so you don't know which one you will get, it's realistic.
-  val broadCastMergeFlow = Flow[HttpRequest, HttpResponse]() {
+  val broadcastMergeFlow = Flow() {
     implicit builder =>
-      //Input -> Splitter -> [ Actions ] -> Merge (take first policy) -> Create an HttpResponse -> Output
-            bCast ~> step1 ~> merge
-      in ~> bCast ~> step2 ~> merge ~> mapStringToResponse ~> out
-            bCast ~> step3 ~> merge
-      (in, out)
+      import FlowGraph.Implicits._
+
+      val bcast = builder.add(bCast3)
+      val merge = builder.add(merge3)
+      val merge1 = builder.add(Merge[HttpResponse](1))
+
+      //TODO: How do we structure this such that the extra merge1 is not needed?
+      bcast ~> step1 ~> merge
+      bcast ~> step2 ~> merge ~> mapStringToResponse ~> merge1
+      bcast ~> step3 ~> merge
+
+      (bcast.in, merge1.out)
   }
 
-  val broadCastZipFlow = Flow[HttpRequest, HttpResponse]() {
+  val broadcastZipFlow = Flow() {
     implicit builder =>
-      //Input -> Splitter -> [ Actions ] -> Zip (combine all policy, ours makes an HttpResponse) -> Output
-            bCast ~> step1 ~> zip.input1
-      in ~> bCast ~> step2 ~> zip.input2 ~> out
-            bCast ~> step3 ~> zip.input3
-      (in, out)
+      import FlowGraph.Implicits._
+
+      val bcast = builder.add(bCast3)
+      val zip = builder.add(zip3)
+
+      bcast ~> step1 ~> zip.in0
+      bcast ~> step2 ~> zip.in1
+      bcast ~> step3 ~> zip.in2
+
+      (bcast.in, zip.out)
   }
+
+// OLD format:
+//
+// //There's a faked delay in step1-3, so you don't know which one you will get, it's realistic.
+//  val broadCastMergeFlow = Flow[HttpRequest, HttpResponse]() {
+//    implicit builder =>
+//      //Input -> Splitter -> [ Actions ] -> Merge (take first policy) -> Create an HttpResponse -> Output
+//            bCast ~> step1 ~> merge
+//      in ~> bCast ~> step2 ~> merge ~> mapStringToResponse ~> out
+//            bCast ~> step3 ~> merge
+//      (in, out)
+//  }
+//
+//  val broadCastZipFlow = Flow[HttpRequest, HttpResponse]() {
+//    implicit builder =>
+//      //Input -> Splitter -> [ Actions ] -> Zip (combine all policy, ours makes an HttpResponse) -> Output
+//            bCast ~> step1 ~> zip.input1
+//      in ~> bCast ~> step2 ~> zip.input2 ~> out
+//            bCast ~> step3 ~> zip.input3
+//      (in, out)
+//  }
 
   def getTickerHandler(tickName: String)(request: HttpRequest): Future[String] = {
     // query the database
@@ -77,6 +105,5 @@ object BootWithFlow extends App with BSONUtils {
       _ map convertToString getOrElse ""
     }
   }
-
 
 }
--- src/main/scala/io/neilord/BootWithHandler.scala
+++ src/main/scala/io/neilord/BootWithHandler.scala
@@ -1,27 +1,36 @@
 package io.neilord
 
 import akka.actor.ActorSystem
-import akka.stream.FlowMaterializer
+import akka.http.scaladsl.Http
+import akka.stream.ActorFlowMaterializer
+import akka.stream.scaladsl.{Sink, Source, Flow}
 import scala.concurrent.Future
-import akka.http.model.HttpMethods._
-import akka.http.model._
-import akka.http._
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model._
 import scala.concurrent.ExecutionContext.Implicits.global
-import akka.stream.scaladsl.Flow
+
 
 object BootWithHandler extends App with BSONUtils {
-  //See http://doc.akka.io/docs/akka-stream-and-http-experimental/1.0-M2/scala/http/server.html
+  //See http://doc.akka.io/docs/akka-stream-and-http-experimental/1.0-RC2/scala/http/server.html
 
   implicit val system = ActorSystem("Streams")
-  implicit val materializer = FlowMaterializer()
+  implicit val materializer = ActorFlowMaterializer()
 
   //Start the server
-  val serverBinding = Http().bind(interface = "localhost", port = 8091)
-  serverBinding.connections.foreach { connection =>
-    println(s"Connection accepted from ${connection.remoteAddress}")
-    connection handleWith { Flow[HttpRequest] mapAsync asyncHandler }
-    //connection.handleWithAsyncHandler(asyncHandler) is equivalent here
-  }
+  val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
+    Http().bind(interface = "localhost", port = 8091)
+
+  //Use:
+  //a Flow[HttpRequest, HttpResponse] for handleWith,
+  //a function HttpRequest => HttpResponse for handleWithSyncHandler,
+  //or a function HttpRequest => Future[HttpResponse] for handleWithAsyncHandler.
+
+  val bindingFuture: Future[Http.ServerBinding] = serverSource.to(Sink.foreach {
+    connection =>
+      println(s"Connection accepted from ${connection.remoteAddress}")
+      connection handleWith { Flow[HttpRequest].mapAsync(1)(asyncHandler) }
+      //connection.handleWithAsyncHandler(asyncHandler) //is equivalent here (runs mapAsync with 1)
+  }).run()
 
   def asyncHandler: HttpRequest => Future[HttpResponse] = {
     //Match specific path. Returns all known tickers
